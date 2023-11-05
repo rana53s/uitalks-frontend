068b2aab9a1bec466d689c56cee83bb41ce7ecbe
Rana Sarkar <rana.sarkarg3@gmail.com>
2023-11-06 01:42:03 +0530

    Mobile View
    Added mobile view for navbar

--- ui-talks/src/app/features/navbar/navbar.component.ts
+++ ui-talks/src/app/features/navbar/navbar.component.ts
@@ -8,24 +8,72 @@ import { AuthService } from 'src/app/core/services';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnChanges {
+  formData: {
+    email: string;
+    password: string;
+  } = {
+    email: '',
+    password: '',
+  };
+  public showLogout = false;
+  public showSigninPage = false;
+  public showSignupPage = false;
+
   constructor(private router: Router, private authService: AuthService) {}
 
-  showLogout = false;
+  isSignedIn = false;
+  showLogin = false;
+
+  toggleLogin() {
+    this.showLogin = !this.showLogin;
+  }
+
+  showSignup = false;
+  toggleSignup() {
+    this.showSignup = !this.showSignup;
+  }
+
+  async submitUserCred() {
+    await this.authService.signUp(this.formData.email, this.formData.password);
+    if (this.authService.isLoggedIn) {
+      this.isSignedIn = true;
+    }
+    if (this.isSignedIn) {
+      setTimeout(() => {
+        this.showSignup = !this.showSignup;
+        this.showLogout = true;
+      }, 1000);
+    }
+  }
+
+  async signIn() {
+    await this.authService.signIn(this.formData.email, this.formData.password);
+    if (this.authService.isLoggedIn) {
+      this.isSignedIn = true;
+    }
+    if (this.isSignedIn) {
+      setTimeout(() => {
+        this.showLogout = true;
+        this.showLogin = !this.showLogin;
+      }, 1000);
+    }
+  }
 
   public logOut() {
     this.authService.logOut();
+    setTimeout(() => {
+      this.showLogout = false;
+    }, 1000);
+  }
+
+  public jumpToLogin() {
+    this.showSignup = !this.showSignup;
+    this.showLogin = !this.showLogin;
+  }
+
+  public goToHome() {
     this.router.navigate(['/home']);
-    this.showLogout = false;
   }
-
-  public loginToPage() {
-    this.router.navigate(['/login']);
-  }
-
-  public subscribeUs() {
-    this.router.navigate(['/subscribe']);
-  }
-
   public goToTopics() {
     this.router.navigate(['/topics']);
   }
